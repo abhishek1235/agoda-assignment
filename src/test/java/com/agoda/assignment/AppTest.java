22332856f2d734f8839bb99bba48ae1258e3fb74
abhishekp <abhishekp@ext.inditex.com>
2019-04-04 16:27:36 +0530

    add one test each for positive and negative scenario

--- src/test/java/com/agoda/assignment/AppTest.java
+++ src/test/java/com/agoda/assignment/AppTest.java
@@ -7,10 +7,16 @@ import org.testng.annotations.Test;
 public class AppTest extends BaseClass
 {
 
-    @DataProvider(name = "Authentication")
-    public static Object[][] credentials() {
+    @DataProvider(name = "ChangePassword_Positive_Scenario")
+    public static Object[][] credentials_Positive_Scenario() {
         return new Object[][]{
                 {"A@quickbrown1fox3jump$over!", "1@Qu!ckBr0wnFoxJumpsOver"},// Valid Password
+        };
+    }
+
+    @DataProvider(name = "ChangePassword_Negative_Scenario")
+    public static Object[][] credentials_Negative_Scenario() {
+        return new Object[][]{
                 {"A@quickbrown1fox3jump$over!", "Pack1mybAxwathfivedAzenajug$"},//Contains 3'a' and 2'A' : No duplicate characters more than 4 times, a and A are considered same in this case
                 {"A@quickbrown1fox3jump$over!", "Pack1mybaxwathfivedazenajug$"},//Contains 5'a' : No duplicate characters more than 4 times, a and A are considered same in this case
                 {"A@quickbrown1fox3jump$over!", "PAck1mybAxwAthfivedAzenAjug$"},//Contains 5'A' : No duplicate characters more than 4 times, a and A are considered same in this case
@@ -23,14 +29,22 @@ public class AppTest extends BaseClass
      * Testng Test
      */
 
-    @Test(priority=0,dataProvider = "Authentication")
-    public void TestPassword( String oldPassword, String newPassword )
+    @Test(priority=0,dataProvider = "ChangePassword_Positive_Scenario")
+    public void TestPassword_Positive_Scenarios( String oldPassword, String newPassword )
     {
-        Boolean flag = ChangePassword(oldPassword,newPassword);
-        if(flag){
-            Assert.assertTrue(ChangePassword(oldPassword,newPassword));
-        }else{
-            Assert.fail("New Password Doesn't Match the expected criteria");
-        }
+       //Positive Scenarios
+       Assert.assertTrue(ChangePassword(oldPassword,newPassword),"New Password Doesn't Match the expected criteria");
+    }
+
+
+    /**
+     * Testng Test
+     */
+
+    @Test(priority=1,dataProvider = "ChangePassword_Negative_Scenario")
+    public void TestPassword_Negative_Scenarios( String oldPassword, String newPassword )
+    {
+        //Negative Scenarios
+        Assert.assertFalse(ChangePassword(oldPassword,newPassword),"New Password Doesn't Match the expected criteria");
     }
 }
