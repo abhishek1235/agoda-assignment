fe4617ff48bbc131cd5c09d0036d324a0ccb4d18
abhishekp <abhishekp@ext.inditex.com>
2019-04-04 16:02:52 +0530

    add testng test and dataprovider

--- src/test/java/com/agoda/assignment/AppTest.java
+++ src/test/java/com/agoda/assignment/AppTest.java
@@ -1,16 +1,32 @@
 package com.agoda.assignment;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class AppTest extends BaseClass
 {
+
+    @DataProvider(name = "Authentication")
+    public static Object[][] credentials() {
+        return new Object[][]{
+                {"OldPassword", "NewPassword"}
+        };
+    }
+
+
     /**
      * Testng Test
      */
 
-    @Test
-    public void Test()
+    @Test(priority=0,dataProvider = "Authentication")
+    public void TestPassword( String oldPassword, String newPassword )
     {
-        System.out.println("Hello World");
+        Boolean flag = ChangePassword(oldPassword,newPassword);
+        if(flag){
+            Assert.assertTrue(ChangePassword(oldPassword,newPassword));
+        }else{
+            Assert.fail("New Password Doesnot Match the expected criteria");
+        }
     }
 }
